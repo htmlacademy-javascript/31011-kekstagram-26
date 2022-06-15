c82226c8decd5d22c5a5aedbd53b6a39acd277d7
Blacking84 <alexey@lugachev.ru>
2022-06-15 19:53:58 +0700

    -

--- js/create-pictures.js
+++ js/create-pictures.js
@@ -1,21 +1,21 @@
 import {getPhotos} from './data.js';
 
-const photoTemplate = document.querySelector('#picture').content;
-const photosContainer = document.querySelector('.pictures');
-
-const photosData = getPhotos();
-
-const photoListFragment = document.createDocumentFragment();
-
-photosData.forEach((photo) => {
-  const photoElement = photoTemplate.cloneNode(true);
-  photoElement.querySelector('.picture__img').src = photo.url;
-  photoElement.querySelector('.picture__likes').textContent = photo.likes;
-  photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
-  photoListFragment.append(photoElement);
-});
-
 function createPictures() {
+  const photoTemplate = document.querySelector('#picture').content;
+  const photosContainer = document.querySelector('.pictures');
+
+  const photosData = getPhotos();
+
+  const photoListFragment = document.createDocumentFragment();
+
+  photosData.forEach((photo) => {
+    const photoElement = photoTemplate.cloneNode(true);
+    photoElement.querySelector('.picture__img').src = photo.url;
+    photoElement.querySelector('.picture__likes').textContent = photo.likes;
+    photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
+    photoListFragment.append(photoElement);
+  });
+
   photosContainer.append(photoListFragment);
 }
 
