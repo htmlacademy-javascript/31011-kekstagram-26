8562d546505f074d4a277a4f7c8135a824735765
Blacking84 <alexey@lugachev.ru>
2022-06-25 22:22:29 +0700

    Масштабирование изображения

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import {createPictures} from './create-pictures.js';
 import {initUploadForm} from './init-upload-form.js';
 import {initUploadFormValidation} from './validate-upload-form.js';
+import {scalePicture} from './scale-picture.js';
 
 createPictures();
 initUploadForm();
 initUploadFormValidation();
+scalePicture();
