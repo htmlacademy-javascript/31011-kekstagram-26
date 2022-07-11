6f224d4da319d749e71e9d79bef2b6907c68e316
Blacking84 <alexey@lugachev.ru>
2022-07-12 06:51:47 +0700

    Защита 12.07.2022 06:51

--- js/api.js
+++ js/api.js
@@ -1,5 +1,7 @@
+const linkData = 'https://26.javascript.pages.academy/kekstagram';
+
 function getData(onSuccess) {
-  fetch('https://26.javascript.pages.academy/kekstagram/data')
+  fetch(`${linkData}/data`)
     .then((response) => response.json())
     .then((pictures) => {
       onSuccess(pictures);
@@ -9,7 +11,7 @@ function getData(onSuccess) {
 function sendData(onSuccess, onFail, body) {
 
   fetch(
-    'https://26.javascript.pages.academy/kekstagram',
+    linkData,
     {
       method: 'POST',
       body: body,
